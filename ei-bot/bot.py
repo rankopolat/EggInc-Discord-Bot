c9b4ee672a9c71cd110777de546ff50399a3279e
Taijitu <116534934+rankopolat@users.noreply.github.com>
2024-02-11 23:41:31 +1100

    Added leadrboard command and spy command

--- ei-bot/bot.py
+++ ei-bot/bot.py
@@ -4,30 +4,36 @@ import discord
 import sqlite3
 import ei_funct as ef
 
+#from discord imports
 from discord import app_commands
 from discord import Embed
 from discord.ext import commands
-from dotenv import load_dotenv
 
+#.env import
+from dotenv import load_dotenv
 
 #ei imports
 import requests
 import ei_pb2
 import base64
 
+
+
+# Load env file gathering Discord Bot Token
 load_dotenv()
-
 TOKEN = os.getenv('DISCORD_TOKEN')
-user_id = os.getenv('USER_ID')
 
 
+
+#Create egg inc main command bot
 bot = commands.Bot(command_prefix='!', intents = discord.Intents.all())
-db = sqlite3.connect('ei-bot/ei-db/kappa.db')
 
+# Connect To the sqlite Database
+db = sqlite3.connect('ei-bot/ei-db/kappa.db')
+#Create db Table if not exists
 ef.create_table(db)
 
-
-
+###Successful Bot Sync Check
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
@@ -41,6 +47,8 @@ async def on_ready():
 
 
 
+
+### /Join command entering EID# to register into the DB
 @bot.tree.command(name="join")
 @app_commands.describe(eid = "Enter EID")
 async def join(interaction: discord.Interaction, eid: str):
@@ -90,6 +98,8 @@ async def join(interaction: discord.Interaction, eid: str):
 
 
 
+
+### Command to display your current informational statistics
 @bot.tree.command(name="info")
 async def info(interaction: discord.Interaction):
 
@@ -120,6 +130,9 @@ async def info(interaction: discord.Interaction):
 
 
 
+
+
+## Command to update your currently stored data to new data
 @bot.tree.command(name="update")
 async def update(interaction: discord.Interaction):
 
@@ -161,6 +174,66 @@ async def update(interaction: discord.Interaction):
     
     else:
         await interaction.response.send_message("Not currently registered in the database please register!")
+    
+
+
+
+
+### Command to view other users stats (nothing private like eid)
+@bot.tree.command(name="spy")
+@app_commands.describe(discord_user = "Enter Discord User TO Spy on")
+async def spy(interaction: discord.Interaction, discord_user: str):
+
+    namecheck = db.execute("SELECT * FROM kappaINFO WHERE username=?", (discord_user,))
+    row = namecheck.fetchone()
+
+    if row is not None:
+
+        user_id = row[1] 
+        periodicals_response = ef.periodical_Requests(user_id)
+
+        ## Get Soul Egg amount and convert to readable form
+        total_soul = ef.numer_formatter(row[3])
+
+        ## Embed discord text bubble
+        embed = Embed(title="Egg Inc Profile",description="A amazingly amazing description of your current egg inc", color=0xffd700)  
+        embed.add_field(name="Discord User", value=discord_user, inline=False)
+        embed.add_field(name="Egg Inc IGN", value=periodicals_response.backup.user_name, inline=False)                                   
+        embed.add_field(name="Soul Eggs", value=total_soul, inline=False)
+        embed.add_field(name="Prophecy Eggs", value=row[4], inline=False)
+        embed.add_field(name="Earning Bonus", value=ef.numer_formatter(row[5]) + "%", inline=False)
+
+        # ephemeral=True to hide
+        await interaction.response.send_message(embed=embed, ephemeral=False)
+
+    else:
+        await interaction.response.send_message(f"{discord_user} does this person even exist???.")
+    
+
+
+
+## Command to Display leaderboard of current registered users Descending Order
+@bot.tree.command(name="lb")
+async def leader_board(interaction: discord.Interaction):
+    list = db.execute("SELECT * FROM kappaINFO ORDER BY earning_bonus DESC")
+    row =  list.fetchall()
+
+    ## Header Points of Embed
+    embed = Embed(title="Kappa Leaderboard",description="The board yes thats right THE BOARD", color=0xffd700)  
+    embed.add_field(name="Rank", value="",inline=True)                                 
+    embed.add_field(name="Egg Inc IGN", value="",inline=True)  
+    embed.add_field(name="Earning Bonus", value="",inline=True)
+
+    for idx,user in enumerate(row):
+
+        ## Adding each player to list Descending Order
+        embed.add_field(name="", value=idx+1,inline=True)                                 
+        embed.add_field(name="", value=user[6],inline=True)  
+        embed.add_field(name="", value=ef.numer_formatter(user[5]) + "%",inline=True)
+
+
+    await interaction.response.send_message(embed=embed, ephemeral=False)
+
 
 
 bot.run(TOKEN)
